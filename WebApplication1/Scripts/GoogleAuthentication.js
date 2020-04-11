b3fd5c0b159efc5eaa59ca51bab7fbd0e7924222
Ramzan <rumkkhan@gmail.com>
2020-04-11 15:38:44 +0530

    Add project files.

---
+++ WebApplication1/Scripts/GoogleAuthentication.js
@@ -0,0 +1,50 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+function getAccessToken() {
+    debugger
+    if (location.hash) {
+        debugger
+        if (location.hash.split('access_token=')) {
+            var accessToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accessToken) {
+                IsUserRegistered(accessToken )
+            }
+        }
+    }
+}
+
+function IsUserRegistered(accessToken) {
+    debugger
+    $.ajax({
+        url: '/api/Account/UserInfo',
+        method: 'GET',
+        headers: {
+            'content-type': 'application/JSON',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            if (response.HasRegistered) {
+                localStorage.setItem('accessToken', accessToken);
+                localStorage.setItem('userName', response.Email);
+                window.location.href = "Data.html";
+            }
+            else {
+                signupExternalUser(accessToken);
+            }
+        }
+    });
+}
+
+function signupExternalUser(accessToken) {
+    $.ajax({
+        url: '/api/Account/RegisterExternal',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function () {
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A57662%2FLogin.html&state=c_8l2HSJSF_3BDG6FSTgWxijIJEoVAe_4frARXvbDuM1";
+        }
+    });
+
+}
